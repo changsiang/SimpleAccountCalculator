b5c109f8b8ac39c75dfb003722d3339e39f18dd2
Lim Siang <limchangsiang@Lims-Mac.local>
2018-02-12 21:54:21 +0800

    Initial Commit

---
+++ SimpleAccountCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleAccountCalculator
+//
+//  Created by Lim Chang Siang on 12/2/18.
+//  Copyright © 2018 Lim Chang Siang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
