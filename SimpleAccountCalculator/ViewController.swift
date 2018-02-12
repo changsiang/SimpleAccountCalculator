e4bcea8a9649a31dc2684d15f5d7d0fdcc3b4bc8
hikari8807 <checkingmymail@gmail.com>
2018-02-12 23:29:14 +0800

    UpdateViewController

--- SimpleAccountCalculator/ViewController.swift
+++ SimpleAccountCalculator/ViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldPrincipal: UITextField!
+    @IBOutlet weak var textFieldNumberOfYears: UITextField!
+    @IBOutlet weak var sliderNumberOfYears: UISlider!
+    @IBOutlet weak var textFieldInterestRate: UITextField!
+    @IBOutlet weak var sliderInterestRate: UISlider!
+    @IBOutlet weak var labelAmount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textFieldPrincipal.text = "10000";
+        textFieldNumberOfYears.text = "10";
+        sliderNumberOfYears.value = 10;
+        textFieldInterestRate.text = "1";
+        sliderInterestRate.value = 1
+    }
+
+    @IBAction func sliderNumberOfYearsChanged( sender: Any){
+        let valueNumberOfYears : Int = Int(sliderNumberOfYears.value);
+        textFieldNumberOfYears.text = String(valueNumberOfYears);
+        self.displayAmount(sender);
+    }
+
+    @IBAction func sliderInterestRateChange(sender: AnyObject){
+        let valueInterestRate: Float = sliderInterestRate.value;
+        textFieldInterestRate.text = NSString(format: "%.2f", valueInterestRate) as String;
+        self.displayAmount(sender);
+    }
+
+    @IBAction func displayAmount(_ sender:Any){
+        let principalAmount: Int = Int(textFieldPrincipal.text)!
+        let numberOfYears: Int = Int(textFieldNumberOfYears.text)!
+        let interestRate: Float = NSString(String: textFieldInterestRate.text!).floatValue / 100;
+        let finalAmount = Double(principalAmount) * pow (Double(1+interestRate), Double(numberOfYears));
+        labelAmount.text = NSString(format: "%.2f", finalAmount) as String;
     }
 
     override func didReceiveMemoryWarning() {
